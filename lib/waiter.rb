1b8ff1040e716bc11f5887ed5b9256dfdc09806b
Evan Burton <evan@heyevan.com>
2020-05-23 16:42:56 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -13,7 +13,7 @@ attr_accesspr :name,
 
  def self.all?
    @@all
- end  
+ end
 
 
  def new_meal(customer, total, tip = 0)
@@ -23,13 +23,16 @@ attr_accesspr :name,
  def meals
   Meal.all.select do |meal|
   meal.waiter == self
+  end
  end
 
  def best_tipper
    best_tipped_meal = meals.max do |meal_a, meal_b|
    meal_a.tip <=> meal_b.tip
+  end
+  best_tipped_meal.customer
  end
 
-best_tipped_meal.customer
+
 
 end
